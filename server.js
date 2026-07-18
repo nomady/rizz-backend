00f4321f323791da4fcf8e3f3bab13fc1b1a2160
Viktor Knezevic <gorunrondovic@gmail.com>
2026-07-18 13:09:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ app.use(cors({
         'http://localhost:3000',
         'http://localhost:5500',
         'http://localhost:8000',
+        'https://rizz-backend-production.up.railway.app',
+        'https://rizzar.xyz',
+        'https://www.rizzar.xyz'
         'http://127.0.0.1:5500',
         'http://127.0.0.1:8000',
         'http://localhost:3001',
