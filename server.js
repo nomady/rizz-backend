4af757650b6c24c5da716e6dea1462c41efa8380
Viktor Knezevic <gorunrondovic@gmail.com>
2026-07-18 13:11:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,16 +23,9 @@ const PORT = process.env.PORT || 3000;
 // ===== CORS CONFIGURATION =====
 app.use(cors({
     origin: [
-        'http://localhost:3000',
-        'http://localhost:5500',
-        'http://localhost:8000',
         'https://rizz-backend-production.up.railway.app',
         'https://rizzar.xyz',
         'https://www.rizzar.xyz'
-        'http://127.0.0.1:5500',
-        'http://127.0.0.1:8000',
-        'http://localhost:3001',
-        'http://127.0.0.1:3000'
     ],
     credentials: true,
     optionsSuccessStatus: 200,
